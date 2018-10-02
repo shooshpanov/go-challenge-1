98787162e2b20738273b67460b26844dd090014a
Sergey S <shooshpanov@yandex.com>
2018-10-02 12:49:07 -0400

    Decode file function implementation

--- decoder.go
+++ decoder.go
@@ -1,12 +1,37 @@
 package drum
 
+import (
+	"io"
+	"os"
+)
+
 // DecodeFile decodes the drum machine file found at the provided path
 // and returns a pointer to a parsed pattern which is the entry point to the
 // rest of the data.
-// TODO: implement
 func DecodeFile(path string) (*Pattern, error) {
-	p := &Pattern{}
+	//file opening
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	//decoding
+	p := new(Pattern)
+	err = NewDecoder(f).Decode(p)
+	if err != nil {
+		return nil, err
+	}
+
 	return p, nil
+}
+
+type Decoder struct {
+	r io.Reader
+}
+
+func NewDecoder(reader io.Reader) *Decoder {
+	return &Decoder{r: reader}
 }
 
 // Pattern is the high level representation of the
